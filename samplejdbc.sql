f7a79577db3fe648dc9bfb74542dc62cfb43417f
Priyadharshini-24 <78081103+Priyadharshini-24@users.noreply.github.com>
2021-12-13 17:55:50 +0530

    Delete samplejdbc.sql

--- samplejdbc.sql
+++
@@ -1,4 +0,0 @@
-create table user_Details(id number primary key,name varchar2(100),email varchar2(100));
-select * from user_Details;
-insert into user_Details values(4,'vishali','vishali@gmail.com');
-commit;
